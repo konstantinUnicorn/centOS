323058f13477addd5519d214ade568aae53cbeb6
Konstantin <konstantin_unicorn@yahoo.com>
2019-12-14 01:30:09 -0500

    switched to dev

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel test application
+                    Laravel dev  application
                 </div>
 
                 <div class="links">
