f43351bd92595c79a311fed0ea19cf46cd5ae3a3
konstantinUnicorn <48957956+konstantinUnicorn@users.noreply.github.com>
2019-12-18 14:13:55 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel test application (test branch)
+                    Laravel (test branch)
                 </div>
 
                 <div class="links">
